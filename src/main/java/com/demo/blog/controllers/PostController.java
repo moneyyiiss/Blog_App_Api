329bc9350a61bd2d44bdf4d2f214085e258da8a9
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-08-15 18:39:08 +0530

    post image uploading api wriiten done

--- src/main/java/com/demo/blog/controllers/PostController.java
+++ src/main/java/com/demo/blog/controllers/PostController.java
@@ -1,10 +1,16 @@
 package com.demo.blog.controllers;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +20,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.demo.blog.config.AppConstants;
 import com.demo.blog.payloads.ApiResponse;
 import com.demo.blog.payloads.PostDto;
 import com.demo.blog.payloads.PostResponse;
+import com.demo.blog.services.FileService;
 import com.demo.blog.services.PostService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @RestController
@@ -27,6 +38,12 @@ public class PostController {
 	
 	@Autowired
 	private PostService postService;
+	
+	@Autowired
+	private FileService fileService;
+	
+	@Value("${project.image}")
+	private String path;
 	
 	//create
 	//http://localhost:9090/api/user/2/category/2/posts this is api in the postman
@@ -65,10 +82,10 @@ public class PostController {
 		//http://localhost:9090/api/posts?pageNumber=0&pageSize=5&sortBy=postId&sortDir=desc
 		@GetMapping("/posts")
 		public ResponseEntity<PostResponse> getAllPost(
-				@RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
-				@RequestParam(value = "pageSize", defaultValue = "5", required = false) Integer pageSize,
-				@RequestParam(value = "sortBy", defaultValue = "postId", required = false) String sortBy,
-				@RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+				@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+				@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+				@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+				@RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
 				){
 			PostResponse postResponse = this.postService.getAllPost(pageNumber, pageSize, sortBy, sortDir);
 			return new ResponseEntity<PostResponse>(postResponse, HttpStatus.OK);
@@ -99,8 +116,39 @@ public class PostController {
 		}
 		
 		
+		// search
+		@GetMapping("/posts/search/{keywords}")
+		public ResponseEntity<List<PostDto>> searchPostByTitle(
+				@PathVariable("keywords") String keywords
+				){
+			List<PostDto> result = this.postService.searchPosts(keywords);
+			return new ResponseEntity<List<PostDto>>(result, HttpStatus.OK);
+		}
 		
+		// post image upload
+		@PostMapping("/post/image/upload/{postId}")
+		public ResponseEntity<PostDto> uploadPostImage(
+				@RequestParam("image") MultipartFile image,
+				@PathVariable Integer postId
+				) throws IOException{
+			String fileName = this.fileService.uploadImage(path, image);
+			PostDto postDto = this.postService.getPostById(postId);
+			postDto.setImageName(fileName);
+			PostDto updatePost = this.postService.updatePost(postDto, postId);
+			return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
+			
+		}
 		
+		// method to serve files
+		@GetMapping(value = "/post/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+		public void downloadImage(
+				@PathVariable("imageName") String imageName,
+				HttpServletResponse response
+				) throws IOException{
+			InputStream resource = this.fileService.getResource(path, imageName);
+			response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+			StreamUtils.copy(resource, response.getOutputStream());
+		}
 		
 		
 		
--- src/main/java/com/demo/blog/repositories/PostRepo.java
+++ src/main/java/com/demo/blog/repositories/PostRepo.java
@@ -3,6 +3,8 @@ package com.demo.blog.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.demo.blog.entities.Category;
 import com.demo.blog.entities.Post;
@@ -11,4 +13,7 @@ import com.demo.blog.entities.User;
 public interface PostRepo extends JpaRepository<Post, Integer>{
 	List<Post> findByUser(User user);
 	List<Post> findByCategory(Category category);
+	
+	@Query("select p from Post p where p.title like :key")
+	List<Post> searchByTitle(@Param("key") String title);
 }
